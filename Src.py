59ffc38ecb007057556a82431462a6a3d9e2d28f
Daniel Pickens <72703981+DanielPickens@users.noreply.github.com>
2021-02-20 17:26:34 -0700

    Src.py
    Yf6f6

--- Src.py
+++ Src.py
@@ -1,11 +1,35 @@
-def is_prime(num):
-    if num > 1: 
-      for i in range(2, num): 
-        if (num % i) == 0: 
-           return num, "is not a prime number"
-        return num, "is a prime number"
-    
 
-user=int(input('enter a number: '))
-a=is_prime(user)
-print(a)
+## this program checks whether a number is a prime number
+
+
+
+num = 11
+ 
+# If given number is greater than 1 start the if statement 
+
+if num > 1:
+ 
+
+    # Iterate from 2 to n / 2
+
+    for i in range(2, num):
+ 
+
+        # If num is divisible by any number between
+
+        # 2 and n / 2, it is not prime
+
+        if (num % i) == 0:
+
+            print(num, "is not a prime number")
+
+            break
+
+    else:
+
+        print(num, "is a prime number")
+ 
+
+else:
+
+    print(num, "is not a prime number")
