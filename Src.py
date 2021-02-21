9a72fcfd4a92d126672090f64981b74945c1a060
Daniel Pickens <72703981+DanielPickens@users.noreply.github.com>
2021-02-20 17:29:51 -0700

    Src.py
    V6c6

--- Src.py
+++ Src.py
@@ -3,21 +3,21 @@
 
 
 
-num = 11
+num = 501
  
-# If given number is greater than 1 start the if statement 
+# If given number is greater than 1
 
 if num > 1:
  
 
-    # Iterate from 2 to n / 2
+    # Iterate from 2 to Prime_num/ 2
 
     for i in range(2, num):
  
 
         # If num is divisible by any number between
 
-        # 2 and n / 2, it is not prime
+        # 2 and Prime_num / 2, it is not prime
 
         if (num % i) == 0:
 
